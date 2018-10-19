ca053536fb0b25899858d2bccc6db9b887137857
Mountaser Halak <mountaser1998@gmail.com>
2018-10-19 18:30:31 +0200

    Create tissot-havoc.mk

---
+++ tissot-havoc.mk
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+<project path="kernel/xiaomi/msm8953" name="mountaser/Moun_Kernel_Tissot" remote="github" revision="9.0" />
+<project path="device/xiaomi/tissot" name="Tissot-Development/android_device_xiaomi_tissot" remote="github" revision="9.0-Havoc" />
+<project path="vendor/xiaomi/tissot" name="Tissot-Development/android_vendor_xiaomi_tissot" remote="github" revision="9.0-test" />
+<project path="device/qcom/common" name="LineageOS/android_device_qcom_common/" remote="github" revision="lineage-15.1" />
+<project path="hardware/qcom/audio-caf-msm8996" name="LiquidRemix/android_hardware_qcom_audio" remote="github" revision="liquid-pie-caf-8996" />
+<project path="hardware/qcom/display-caf-msm8996" name="LiquidRemix/android_hardware_qcom_display" remote="github" revision="liquid-pie-caf-8996" />
+<project path="hardware/qcom/media-caf-msm8996" name="LiquidRemix/android_hardware_qcom_media" remote="github" revision="liquid-pie-caf-8996" />
+<project path="hardware/qcom/keymaster" name="LiquidRemix/android_hardware_qcom_keymaster" remote="github" revision="pie" />
+</manifest>
