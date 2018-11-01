7068358b61fcfc487aaca75199fde00acbb07eed
Mountaser Halak <mountaser1998@gmail.com>
2018-11-01 15:54:34 +0100

    Create tissot-los.mk

---
+++ tissot-los.mk
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+<project path="kernel/xiaomi/msm8953" name="mountaser/Moun_Kernel_Tissot" remote="github" revision="9.0" />
+<project path="device/xiaomi/tissot" name="Tissot-Development/android_device_xiaomi_tissot" remote="github" revision="9.0-test-2" />
+<project path="vendor/xiaomi/tissot" name="Tissot-Development/android_vendor_xiaomi_tissot" remote="github" revision="9.0-test-2" />
+<project path="device/qcom/common" name="LineageOS/android_device_qcom_common/" remote="github" revision="lineage-15.1" />
+</manifest>
